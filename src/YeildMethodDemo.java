9c1322ed8d9b165b84a761f0460f1dc9d8d6011d
kunal nagwanshi <nagwanshikunal23@gmail.com>
2024-10-20 19:05:14 +0530

    hellllllo

---
+++ src/YeildMethodDemo.java
@@ -0,0 +1,21 @@
+public class YeildMethodDemo {
+    public static void main(String[] args) {
+
+        MyThread t = new MyThread();
+        t.start();
+        for (int i = 0; i < 11; i++) {
+            System.out.println("main thread");
+        }
+
+    }
+}
+class MyThread extends Thread{
+    public void run(){
+        for (int i = 0; i < 11; i++) {
+            System.out.println("child thread");
+        }
+        Thread.yield();
+    }
+
+
+}
